292469bf66d10469d72470d7b4e64f1e4fb5aaac
Madhan M <166613405+Madhanmohan11@users.noreply.github.com>
2025-03-26 23:28:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import Services from './Components/Services/Services';
 import MyWork from './Components/MyWork/MyWork';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
-
+ 
+ 
 const App = () => {
   const [loading, setLoading] = useState(true);
 
@@ -15,18 +16,23 @@ const App = () => {
     // Simulate loading time
     setTimeout(() => {
       setLoading(false);
-    }, 3000); // 3 seconds delay before showing the website
+    }, 9025); // 9 seconds delay before showing the website
   }, []);
 
   return (
     <>
+
+      
       {loading ? (
         <Preloader />
+        
       ) : (
         <div>
+         
           <Navbar />
           <Hero />
           <About />
+
           <Services />
           <MyWork />
           <Contact />
