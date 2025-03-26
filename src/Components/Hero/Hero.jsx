eaa1fef6b5bb02bb32efe8048645ea2946c8de3c
Madhan M <166613405+Madhanmohan11@users.noreply.github.com>
2025-03-27 00:07:29 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
         <div className="hero-resume">
-        <a href="/src/assets/imgs/Resume_MM.pdf.pdf" target="_blank" rel="noopener noreferrer">My Resume</a></div>
+        <a href="./src/assets/imgs/Resume_MM.pdf.pdf" target="_blank" rel="noopener noreferrer">My Resume</a></div>
       </div>
       <div><Share></Share></div>
     </div>
